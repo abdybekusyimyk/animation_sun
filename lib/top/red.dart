d815a596df53640aa42de832be23612cebf06732
abdybekusyimyk <abdybekuulusyimyk@gmail.com>
2023-01-14 23:10:09 +0600

    comit

---
+++ lib/top/red.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Red extends StatelessWidget {
+  const Red({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+        ],
+      ),
+    );
+  }
+}
